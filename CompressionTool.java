a676da72c8d5d575caa1b6393c9c413ce2c4119b
unknown <edwardarahi2002@gmail.com>
2025-12-11 05:19:16 +0100

    making huffman tree class

--- CompressionTool.java
+++ CompressionTool.java
@@ -1,10 +1,8 @@
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.TreeMap;
 
 public class CompressionTool{
@@ -13,22 +11,33 @@ public class CompressionTool{
         this.fileUrl = fileUrl;
     }
     public TreeMap<Character,Integer> charactersOcurrences(){
-        try (File file = new File(fileUrl); FileInputStream in = new FileInputStream(file);
-        BufferedInputStream bf = new BufferedInputStream(file)) {
-            while (bf.rea d() != -1) {
-                
-            }
-
+        TreeMap<Character,Integer> map = new TreeMap<>();
+        try (BufferedInputStream bf = new BufferedInputStream(new FileInputStream(new File(fileUrl)))) {
             
+            while (true) {
+                int b = bf.read();
+                if(b == -1) break;
+                Character ch = (char)b;
+                if(map.containsKey(ch)){
+                    map.put(ch,map.get(ch)+1);
+                }
+                else{
+                    map.put(ch,0);
+                }
+            }            
         } catch (FileNotFoundException e) {
-            System.err.println("file not found",e.getMessage());
+            System.err.println("file not found");
         }
         catch(IOException e){
-            System.err.println("error while reading the file",e.getMessage());
+            System.err.println("error while reading the file");
         }
+        return map;
     }
     public static void main(String[] args){
-
+        TreeMap<Character,Integer> treeMap = new TreeMap<>();
+        String url = new String("c:/Users/admin/Desktop/New folder/pentesting.txt");
+        treeMap = (new CompressionTool(url)).charactersOcurrences();
+        System.out.println(treeMap);
     }
 }
 
